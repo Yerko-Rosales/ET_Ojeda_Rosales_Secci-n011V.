6c6a814a33947cd73a9e90bfa4dcbd9700511176
Yerko Rosales B. <yer.rosales@duocuc.cl>
2024-12-18 21:09:53 -0300

    Añadir nueva carpeta apk

--- MyProyect/src/environments/environment.prod.ts
+++ MyProyect/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:'http://localhost:3000'
+  apiUrl:'http://localhost:3000',
+  apiurl: 'https://repo-progra-3.onrender.com'
 };
--- MyProyect/src/environments/environment.ts
+++ MyProyect/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl:'http://localhost:3000'
+  apiUrl:'http://localhost:3000',
+  apiurl: 'https://repo-progra-3.onrender.com'
 };
 
 /*
